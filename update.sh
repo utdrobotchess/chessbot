7fc2b6aaacf00dbf7d94d8bf1394f12c38055ad0
Omar Hasan <omarhasan777@gmail.com>
2015-04-11 12:21:06 -0500

    Updated the update shell script to include the src/test/forcesensor-test repository

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 cp ./src/chess-test/XB* ./src/test/xbee-test/
 cp ./src/chess-test/Wheel.* ./src/chess-test/PIDController.* ./src/chess-test/digitalWriteFast.h ./src/test/wheel-test/
+cp ./src/chess-test/Wheel.* ./src/chess-test/PIDController.* ./src/test/forcesensor-test/
 cp ./src/chess-test/Gyroscope.* ./src/test/gyroscope-test/
 cp ./src/chess-test/Photodiode.* ./src/test/photodiode-test/
+cp ./src/chess-test/ForceSensor.h ./src/test/forcesensor-test/
